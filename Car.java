a930d257502236a0e17a1c7db1570c4f978164f0
An-eesha <anishagotame06@gmail.com>
2026-06-17 21:40:00 +0545

    commite new thing

--- Car.java
+++ Car.java
@@ -5,7 +5,7 @@ class Car {
   
     // public void setDetail(String brandName, String model, int price){
     // by using constructor 
-    public Car (String brandName, String model, int price){
+    public Car (){
       this.brandName= brandName;
       this.model= model;
       this.price= price;
--- test.java
+++ test.java
@@ -1,10 +1,19 @@
 public class test {
     public static void main(String [] args){
         Car c = new Car("BYD", "Atto ", 1000);    //by using constructor
+        String carDetail =c.getDetail();
+        System.out.println(carDetail);
 
+        Car c1 = new Car("BYD", "Atto ", 1000);    //by using constructor
+        String carDetail =c1.getDetail();
+        System.out.println(c1Detail);
+        
+        Car c2 = new Car("BYD", "Atto ", 1000);    //by using constructor
+        String carDetail =c2.getDetail();
+        System.out.println(c2Detail);
+        
         // c.setDetail("BYD", "Atto", 40000); // without constructor
         // String carDetail=c.getDetail();
-    
-        System.out.println(c.getDetail());
+        // System.out.println(c.getDetail());
     }
 }
