d3003aaef13e8ee2a33c0470fa0fc633de65606f
An-eesha <anishagotame06@gmail.com>
2026-06-17 22:01:14 +0545

    All set

---
+++ Solve1/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Enter age: ");
+            int age = sc.nextInt();
+
+            if (age < 18) {
+                throw new InvalidAgeException("Age must be 18 or above.");
+            }
+
+            System.out.println("Valid age.");
+        } catch (InvalidAgeException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
