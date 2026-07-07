d4b5c72a60ded1c787240d6059be12cd3fb42c93
An-eesha <anishagotame06@gmail.com>
2026-07-07 17:53:54 +0545

    comment

--- test.java
+++ test.java
@@ -4,16 +4,16 @@ public class test {
         String carDetail =c.getDetail();
         System.out.println(carDetail);
 
-        Car c1 = new Car("BYD", "Atto ", 1000);    //by using constructor
-        String carDetail =c1.getDetail();
-        System.out.println(c1Detail);
+        // Car c1 = new Car("BYD", "Atto ", 1000);    //by using constructor
+        // String carDetail =c1.getDetail();
+        // System.out.println(c1Detail);
         
-        Car c2 = new Car("BYD", "Atto ", 1000);    //by using constructor
-        String carDetail =c2.getDetail();
-        System.out.println(c2Detail);
+        // Car c2 = new Car("BYD", "Atto ", 1000);    //by using constructor
+        // String carDetail =c2.getDetail();
+        // System.out.println(c2Detail);
         
         // c.setDetail("BYD", "Atto", 40000); // without constructor
         // String carDetail=c.getDetail();
-        // System.out.println(c.getDetail());
+        //  System.out.println(c.getDetail());
     }
 }
